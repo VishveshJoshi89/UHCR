1418efab999f7c48ad895548c150e2cb7d36b669
VishveshJoshi89 <vishveshj8989@gmail.com>
2026-06-09 00:52:27 +0530

    setup github pages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
 
- base:"/UHCR/",
+  base:'/UHCR/',
 
- plugins:[react()]
+  plugins:[
+    react()
+  ]
 
 })
